d066b590212b652fc3872b1b53013be79108d697
santiagoarbelaezc <santiago.arbelaezc@uqvirtual.edu.co>
2025-11-09 13:44:45 -0500

    cambiando a https por errores

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://todotechback.us-east-2.elasticbeanstalk.com', 
+  apiUrl: 'https://todotechback.us-east-2.elasticbeanstalk.com',  // ← HTTPS
   stripePublishableKey: 'pk_test_51SQ9bHRpZZ0VaxsYA75UGBMvjgNw2sprW6qC4WQDTyAkonmOHxPvB0HJ33xvh0v7XhhTflP3LsEyQINelK3iQeMk00djhDWdke'
 };
