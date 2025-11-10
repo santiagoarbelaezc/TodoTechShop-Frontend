fd33f933c38aa2cc3f733f33586a8360dbcf757c
santiagoarbelaezc <santiago.arbelaezc@uqvirtual.edu.co>
2025-11-10 00:39:14 -0500

    cambiando apiurl por la del back en aws

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080', 
+  apiUrl: 'http://todotechback.us-east-2.elasticbeanstalk.com', 
   stripePublishableKey: 'pk_test_51SQ9bHRpZZ0VaxsYA75UGBMvjgNw2sprW6qC4WQDTyAkonmOHxPvB0HJ33xvh0v7XhhTflP3LsEyQINelK3iQeMk00djhDWdke'
 };
