418d6cef510f344b769c94ca17a86d9edf652d02
santiagoarbelaezc <santiago.arbelaezc@uqvirtual.edu.co>
2025-11-11 00:05:35 -0500

    redireccion http a https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://todotechback.us-east-2.elasticbeanstalk.com', 
+  apiUrl: 'https://d2jctboz5xbevf.cloudfront.net',  // ← CAMBIA ESTA LÍNEA
   stripePublishableKey: 'pk_test_51SQ9bHRpZZ0VaxsYA75UGBMvjgNw2sprW6qC4WQDTyAkonmOHxPvB0HJ33xvh0v7XhhTflP3LsEyQINelK3iQeMk00djhDWdke'
 };
